49d243f5856ec987dcf544008ec59710348af53a
hevok <age@liv.ac.uk>
2012-09-16 16:13:23 +0200

    Uploading content and adding temporary files to ignore list.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='django-polls',
+    version='0.1',
+    packages=['polls'],
+    package_data={'polls': ['templates/polls']},
+    license='',
+    long_description=open('README.rst').read(),
+    url='http://www.denigma.de',
+    author='Hevok',
+    author_email='hevok@denigma.de'
+)
